af2b233bf3377d79a892a9b07108055b06915c7e
liou <liou@pop-os.localdomain>
2025-12-26 20:43:57 +0800

    if has different user name

--- flake.nix
+++ flake.nix
@@ -9,14 +9,25 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs =
+    { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux"; # 如果是 ARM 架构则改为 "aarch64-linux"
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."liou" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ ./home-manager/home.nix ];
+    in
+    {
+      homeConfigurations = {
+        # 原有的配置
+        "liou" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home-manager/home.nix ];
+        };
+
+        # 新环境的配置
+        # "otheruser" = home-manager.lib.homeManagerConfiguration {
+        #   inherit pkgs;
+        #   modules = [ ./home-manager/home.nix ]; # 可以指向不同的 home.nix
+        # };
       };
     };
 }
